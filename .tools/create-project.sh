c12657f4e5ca2fbe72266d1cce2e803d5104c2f6
qnnp <wjp@pfan.ltd>
2024-08-10 12:37:49 +0800

    ``` 增加项目和包名提示，并实现模板克隆功能
    在创建项目的脚本中，添加了提示用户输入项目名和包名的功能。同时，实现了通过Git克隆模板仓库的功能，以支持定制化的项目初始化。
    ```

--- .tools/create-project.sh
+++ .tools/create-project.sh
@@ -7,15 +7,34 @@ WORKDIR=$(pwd)
 PROJECT_NAME=$1
 PACKAGE_NAME=$2
 
+read -r -p "Enter project name: " PROJECT_NAME
 if [ -z "$PROJECT_NAME" ]; then
-  echo "Please provide a project name"
-  exit 1
-fi
-if [ -z "$PACKAGE_NAME" ]; then
-  PACKAGE_NAME="com.${PROJECT_NAME}"
+  PROJECT_NAME="TaroRN"
 fi
 
-# npx react-native init "$PROJECT_NAME" --package-name="$PACKAGE_NAME"
+if [ -d "$PROJECT_NAME" ]; then
+  echo "$PROJECT_NAME already exists"
+  exit 1
+fi
+
+read -r -p "Enter package name: " PACKAGE_NAME
+if [ -z "$PACKAGE_NAME" ]; then
+  echo "Please provide a package name"
+  exit 1
+fi
+
+# clone template
+git_exists=$(which git)
+
+if [ -z "$git_exists" ]; then
+  echo "git not found"
+  exit 1
+fi
+
+git clone https://github.com/qnnp-me/taro-rn-template.git "$PROJECT_NAME"
+rm -rf "$PROJECT_NAME/.git"
+
+npx react-native init "$PROJECT_NAME" --package-name="$PACKAGE_NAME"
 
 file_path="${WORKDIR}/.tools/update-version.sh"
 # 检查文件是否存在
